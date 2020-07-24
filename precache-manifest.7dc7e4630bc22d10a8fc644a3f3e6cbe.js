381fef2b463b9db6c0c1e689a824eee183006dd7
Kyle Hays <38229188+CruddyShad0w@users.noreply.github.com>
2020-07-24 16:13:10 -0700

    Updates

---
+++ precache-manifest.7dc7e4630bc22d10a8fc644a3f3e6cbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "e46d6f4a175457739aa3",
+    "url": "/Portfolio/static/js/main.e46d6f4a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "e46d6f4a175457739aa3",
+    "url": "/Portfolio/static/css/main.c511cc70.chunk.css"
+  },
+  {
+    "revision": "13303b2d1f42a2f614b3a15489bcb887",
+    "url": "/Portfolio/index.html"
+  }
+];
